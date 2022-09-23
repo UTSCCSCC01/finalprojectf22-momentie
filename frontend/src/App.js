87b26e2870d690820faf7a09e3874d8c81238ed2
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2022-09-23 16:23:00 -0400

    Created connection with backend
    By clicking a button, the backend will return a text saying you are in, which we will console.log.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
-function button_on_click(){
+function button_on_click(callback) {
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:5000/login");
+  xhr.open("POST", "http://localhost:5000/login?username=candy&password=cane");
 
   xhr.setRequestHeader("Accept", "application/json");
   xhr.setRequestHeader("Content-Type", "application/json");
 
   xhr.onload = () => console.log(xhr.responseText);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      callback(xhr);
+    }
+  }
 
-  let data = `{
-    "username": "candy",
-    "password": "cane",
-  }`;
-
-  xhr.send(data);
+  xhr.send();
 }
 
 function App() {
   return (
     <div className="App">
-      <button
-        onClick={button_on_click}
+      <button id="dummy"
+        onClick={() => button_on_click(callback)}
       >
-        "click me!!!!!!!"   
+        "click me!!!!!!!"
       </button>
-      
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
