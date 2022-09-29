b0b22a8a469cfec82c07dad4bed15b733c22ec6c
Oliiiv <weng.zhang0708@gmail.com>
2022-09-29 10:56:59 -0400

    add a folder for all learning artifacts members have completed

---
+++ learning-tasks/ajax_req/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/server', (req, resp)=>{
+    resp.setHeader('Access-Control-Allow-Origin','*');
+    resp.send('HELLO AJAX!!');
+});
+
+app.listen(8000,()=>{
+    console.log("8000 listening...");
+});
---
+++ learning-tasks/express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, resp)=>{
+    resp.send('HELLO EXPRESS!!');
+});
+
+app.listen(8000,()=>{
+    console.log("8000 listening...");
+});
