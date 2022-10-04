01b740c793a80e8076a85fc772a1c521e04aefd0
Oliiiv <weng.zhang0708@gmail.com>
2022-10-03 21:32:28 -0400

    reconstruct router

--- backend/src/controller/user/userController.ts
+++ backend/src/controller/user/userController.ts
@@ -1,11 +1,51 @@
 /* Exports function that initializes API calls on express  */
 // import * from './userAuth';
-import UserModel, { User } from "../userModel";
+import UserModel, { User } from '../../model/userModel';
 import { Express } from 'express'
+import { time, timeStamp } from 'console';
+import passport from "passport";
+import passportLocal from "passport-local";
+// import UserModel from '../../model/userModel';
 
 const userLogin = (req: any, res: any) => {
     console.log(req.user)
     return res.send("You are in111!");
 }
 
-module.exports = { userLogin }
+interface IReturnRegister{
+    user: User | null;
+    error: boolean;
+    message: string;
+}
+
+const userSignUp = async ({email, username, password}:{email: string, username: string, password: string}) : Promise<IReturnRegister> => {
+    const user = await UserModel.findOne({email: email});
+    if(user){
+        return {
+            message: "User already exists",
+            error: true,
+            user: null
+        }
+    };
+    const newUser = new UserModel ({
+        email,
+        username
+    });
+    let signUpUser = await UserModel.register(newUser, password);
+    console.log("user registered");
+    if (signUpUser){
+        return {
+            user: signUpUser as User,
+            message: "Sign up successfully",
+            error: false
+        }
+    }else{
+        return {
+            user: null,
+            message: "Sign up error",
+            error: true
+        }
+    }
+};
+
+module.exports = { userLogin, userSignUp }
---
+++ backend/src/routes/account.ts
@@ -0,0 +1,19 @@
+import { User } from '../model/userModel';
+
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const { userLogin, userSignUp } = require('../controller/user/userController');
+// const { userLogOut } = require('../controller/user/userController');
+
+router.get('/', (req: any, res: any) => {
+  res.status(200).send('login page')
+});
+
+// router.post('/login', passport.authenticate('local'), (req: any, res: any) => {userLogin(req, res)});
+router.post('/login', passport.authenticate('local'), userLogin );
+
+router.post('/signup', userSignUp);
+
+module.exports = router
