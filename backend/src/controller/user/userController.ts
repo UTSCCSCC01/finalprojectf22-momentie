135b1b47e99d157e06260a99b943afbf647745f5
Oliiiv <weng.zhang0708@gmail.com>
2022-10-04 17:38:38 -0400

    MOM-28 import reivsed

--- backend/src/controller/user/userController.ts
+++ backend/src/controller/user/userController.ts
@@ -1,12 +1,6 @@
 /* Exports function that initializes API calls on express  */
 // import * from './userAuth';
 import UserModel, { User } from '../../model/userModel';
-import { Express } from 'express'
-import { time, timeStamp } from 'console';
-import passport from "passport";
-import passportLocal from "passport-local";
-// import UserModel from '../../model/userModel';
-import passportLocalMongoose from 'passport-local-mongoose';
 
 const userLogin = (req: any, res: any) => {
     console.log(req.user)
