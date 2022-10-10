f0ab22c36a549a900fe6c137efb2ad450a07fe72
Shaopeng (Chris) Lin <shaopeng.lin@mail.utoronto.ca>
2022-10-09 23:51:27 -0400

    MOM-27 - Reimplemented Login Page for React
    Login now works exactly the same as before but with react. The first place users go to now is this page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function button_on_click() {
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:5000/account/login?email=candy@gmail.com&password=cane");
-
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  xhr.onload = () => console.log(xhr.responseText);
-
-  xhr.send();
-}
-
+import { Route, Routes } from 'react-router-dom'
+import Login from '../src/components/pages/Login/Login.jsx'
 function App() {
-  return (
-    <div className="App">
-      <button id="dummy"
-        onClick={() => button_on_click()}
-      >
-        "click me!!!!!!!"
-      </button>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" />
+      <Route path="/signup" />
+      <Route path="/profile" />
+    </Routes>
   );
 }
 
