9171c39eb74a3a85f816a36ce93396b0cf28326c
Oliiiv <weng.zhang0708@gmail.com>
2022-10-15 08:22:06 -0500

    MOM-58 modify timelineModel schema

--- backend/src/model/timelineModel.ts
+++ backend/src/model/timelineModel.ts
@@ -5,6 +5,10 @@ export interface Timeline extends mongoose.Document {
     email: string;
     experienceTitle: string;
     experienceContent: string;
+    title: string;
+    content: string;
+    startTime?: string;
+    endTime?: string;
     createdAt: string;
     updatedAt: string;
 };
@@ -13,6 +17,10 @@ const Timeline = new Schema ({
     email: { type: String, ref: 'profileData', require: true },
     experienceTitle: { type: String },
     experienceContent: { type: String },
+    title: { type: String },
+    content: { type: String },
+    startTime: { type: String },
+    endTime: { type: String },
 },{ timestamps: true });
 
 const TimelineModel = mongoose.model('timelineData', Timeline);
