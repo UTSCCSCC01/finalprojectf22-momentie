259e72bde586ab8e40516d5f186ee71fdd47c585
Xuen Shen <xuenshen@gmail.com>
2022-10-18 22:48:40 -0400

    MOM-60 fixed syntax after solving conflict
    left extra line "edit_profile, rate_profile } = require('../controller/profile/profileController')" when solved conflict

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -3,7 +3,6 @@ var router = express.Router();
 
 const { retrieve_profile,
   edit_profile, likeRetri, rate_profile } = require('../controller/profile/profileController');
-  edit_profile, rate_profile } = require('../controller/profile/profileController')
 
 router.get('/', retrieve_profile)
 router.patch('/', edit_profile)
