8ee7ac7be0c4452f8d2173ef895e945dad557250
Xu Zheng <barry.zheng@mail.utoronto.ca>
2022-10-19 19:47:51 -0400

    MOM-63: implement RESTAPI for deleting user tags

--- backend/src/routes/tag.ts
+++ backend/src/routes/tag.ts
@@ -1,11 +1,11 @@
 const express = require('express');
 var router = express.Router();
 
-const { userTagCreate } = require('../controller/tag/userTagController');
+const { userTagCreate, userTagDelete } = require('../controller/tag/userTagController');
 const { tagRetri } = require('../controller/tag/tagController');
 
 router.post('/', userTagCreate);
-
+router.delete('/', userTagDelete)
 router.get('/', tagRetri);
 
 export = router
